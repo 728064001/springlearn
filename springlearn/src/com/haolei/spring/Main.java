f8e79e88163e5d13e9317a00459848457f9034b2
haolei <728064001@qq.com>
2014-12-03 23:59:48 +0800

    References and cascade configuration

--- springlearn/src/com/haolei/spring/Main.java
+++ springlearn/src/com/haolei/spring/Main.java
@@ -18,5 +18,8 @@ public class Main {
 		
 		Car car2 = (Car)ctx.getBean("car2");
 		System.out.println(car2);
+		
+		Person person = (Person)ctx.getBean("person");
+		System.out.println(person);
 	}
 }
